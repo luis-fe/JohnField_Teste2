d6476e29c858dc97d124591a73b4693e8913aa72
luis-fe <luisengp@gmail.com>
2024-05-15 10:21:21 -0300

    ORGANIZACAO DE ARQUIVOS

---
+++ Service/Operacoes/Opercao.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import ConexaoPostgreMPL
+
+
+def BuscarOperacoes():
+    conn = ConexaoPostgreMPL.conexaoJohn()
+
+    consulta = """
+    select  c.*, f."nomeFase" from "Easy"."Operacao" c
+    inner join "Easy"."Fase" f on f."codFase" = c."codFase"
+    """
+
+    consulta = pd.read_sql(consulta,conn)
+    conn.close()
+
+    return consulta
+
+def BuscarCategoriaEspecifica(codcategoria):
+    conn = ConexaoPostgreMPL.conexaoJohn()
+
+    consulta = """
+    select c.codcategoria , c."nomeCategoria"  from "Easy"."Categoria" c  
+    where c.codcategoria = %s
+    """
+
+    consulta = pd.read_sql(consulta,conn,params=(codcategoria,))
+    conn.close()
+
+    return consulta
+
+def InserirCategoria(codcategoria, nomeCategoria):
+    consulta = BuscarCategoriaEspecifica(codcategoria)
+
+    if consulta.empty:
+        conn = ConexaoPostgreMPL.conexaoJohn()
+        inserir = """
+        insert into "Easy"."Categoria" (codcategoria , "nomeCategoria") values ( %s, %s )
+        """
+        cursor = conn.cursor()
+        cursor.execute(inserir,(codcategoria, nomeCategoria,))
+        conn.commit()
+        cursor.close()
+
+        conn.close()
+
+        return pd.DataFrame([{'Mensagem': "Categoria cadastrado com Sucesso!", "status": True}])
+
+    else:
+        return pd.DataFrame([{'Mensagem': "Categoria já´existe!", "status": False}])
+
+def UpdateCategoria(codcategoria, nomeCategoria):
+
+    consulta = BuscarCategoriaEspecifica(codcategoria)
+
+    if consulta.empty:
+        return pd.DataFrame([{'Mensagem':"Categoria Nao encontrado!","status":False}])
+    else:
+        CategoriaAtual = consulta['nomeCategoria'][0]
+        if CategoriaAtual == nomeCategoria :
+            nomeCategoria = CategoriaAtual
+
+
+        conn = ConexaoPostgreMPL.conexaoJohn()
+        update = """
+        update "Easy"."Categoria"
+        set  "nomeCategoria" = %s 
+        where "codcategoria" = %s 
+        """
+
+        cursor = conn.cursor()
+        cursor.execute(update,(nomeCategoria,codcategoria,))
+        conn.commit()
+        cursor.close()
+
+        conn.close()
+        return pd.DataFrame([{'Mensagem': "Categoria Alterado com Sucesso!", "status": True}])
--- routes/__init__.py
+++ routes/__init__.py
@@ -4,16 +4,17 @@ from flask import Blueprint
 routes_blueprint = Blueprint('routes', __name__)
 
 # Importe as rotas dos arquivos individuais
-from .usuariosJonhField import usuarios_routesJohn 
-from .clenteJohnField import cliente_routesJohn
-from .categoriaJohnField import categoria_routesJohn
-from .FaseJohnField import fase_routesJohn
-from .gradePadrao import gradePadrao_routesJohn
-from .GeracaoOP import GeraoOP_routesJohn
-from .MovimentacaoOP import MovimentaoOP_routesJohn
-from .Formulario import formulario_routesJohn
-from .RoteiroPadrao import roteiroPadrao_routesJohn
-from .Dashboard import Dashboard_routesJohn
+from .Usuarios.usuariosJonhField import usuarios_routesJohn
+from .Clientes.clenteJohnField import cliente_routesJohn
+from .Categorias.categoriaJohnField import categoria_routesJohn
+from .Fases.FaseJohnField import fase_routesJohn
+from .Tamanhos.gradePadrao import gradePadrao_routesJohn
+from .Ops.GeracaoOP import GeraoOP_routesJohn
+from .Ops.MovimentacaoOP import MovimentaoOP_routesJohn
+from .Ops.Formulario import formulario_routesJohn
+from .Fases.RoteiroPadrao import roteiroPadrao_routesJohn
+from .Dashboard.Dashboard import Dashboard_routesJohn
+from .Operacoes.operacao import operacao_routesJohn
 
 # Registre as rotas nos blueprints
 routes_blueprint.register_blueprint(usuarios_routesJohn)
@@ -26,3 +27,4 @@ routes_blueprint.register_blueprint(MovimentaoOP_routesJohn)
 routes_blueprint.register_blueprint(formulario_routesJohn)
 routes_blueprint.register_blueprint(roteiroPadrao_routesJohn)
 routes_blueprint.register_blueprint(Dashboard_routesJohn)
+routes_blueprint.register_blueprint(operacao_routesJohn)
