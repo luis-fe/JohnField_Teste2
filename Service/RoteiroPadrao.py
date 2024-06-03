62dc4cd9f72b12a3edfccde167a3b2135a6663ef
luis-fe <luisengp@gmail.com>
2024-06-03 16:03:14 -0300

    requi

--- Service/RoteiroPadrao.py
+++ Service/RoteiroPadrao.py
@@ -5,6 +5,7 @@ from Service import FaseJohnField
 def BuscarRoteiros():
   consulta = """
   SELECT "codRoteiro", "nomeRoteiro", "codFase" FROM "Easy"."Roteiro"
+  order by "codRoteiro", "codFase" asc
   """
   conn = ConexaoPostgreMPL.conexaoJohn()
   consulta = pd.read_sql(consulta, conn)
