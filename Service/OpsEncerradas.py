7e771cdfb3dcaf58b772b55f0a5d850d11258c75
luis-fe <luis_engp@hotmail.com>
2024-06-16 17:51:52 -0300

    api ConsultaRoteiroOP

--- Service/OpsEncerradas.py
+++ Service/OpsEncerradas.py
@@ -6,7 +6,8 @@ import ConexaoPostgreMPL
 
 def RelatorioEncerramento(dataInicio, dataFinal):
     consulta  ="""
-    select * from railway."Easy"."OpsEncerradas" oe
+    select "idOP", dataencerramento::varchar as dataencerramento, u."nomeUsuario" as usuario_encerramento  from railway."Easy"."OpsEncerradas" oe
+    inner join "Easy"."Usuario" u on u.idusuario = oe.usuario_encerramento
     where dataencerramento >= %s and dataencerramento <=%s
     """
 
@@ -19,5 +20,12 @@ def RelatorioEncerramento(dataInicio, dataFinal):
         consulta2 = pd.read_sql(consulta2,conn)
 
         consulta = pd.merge(consulta,consulta2,on='idOP',how='left')
+
+    consulta['dataencerramento'] = consulta['dataencerramento'].apply(converterData)
     return consulta
 
+def converterData(date_str):
+    year = date_str[:4]
+    month = date_str[5:7]
+    day = date_str[8:]
+    return f"{day}/{month}/{year}"
