3a6ce4e27b040ad0a6b6aee29b08924c1fd57352
luis-fe <luis_engp@hotmail.com>
2024-06-16 21:34:28 -0300

    Operacoes 2

--- Service/Operacoes/Opercao.py
+++ Service/Operacoes/Opercao.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import ConexaoPostgreMPL
-from Service import FaseJohnField
+from Service import FaseJohnField, CategiaJohnField
 
 def BuscarOperacoes():
     conn = ConexaoPostgreMPL.conexaoJohn()
 
     consulta = """
-        select  c.*, f."nomeFase",c2."nomeCategoria"  ,to2."tempoPadrao"  from "Easy"."Operacao" c
+        select  c.*, f."nomeFase",c2."nomeCategoria"  ,to2."tempoPadrao" as "TempoPadrao(s)", f."nomeFase", "Maq/Equipamento"  from "Easy"."Operacao" c
     inner join "Easy"."Fase" f on f."codFase" = c."codFase"
     inner join "Easy"."TemposOperacao" to2 on to2."codOperacao" = c."codOperacao" 
     inner join "Easy"."Categoria" c2 on c2.codcategoria = to2."codCategoria" 
@@ -15,42 +15,51 @@ def BuscarOperacoes():
     consulta = pd.read_sql(consulta,conn)
     conn.close()
 
+    consulta['Pcs/Hora'] = 3600/consulta['TempoPadrao(s)']
+
     return consulta
 
-def BuscarOperacaoEspecifica(codOperacao):
+def BuscarOperacaoEspecifica(nomeOperacao):
     conn = ConexaoPostgreMPL.conexaoJohn()
 
     consulta = """
     select c.*  from "Easy"."Operacao" c  
-    where c."codOperacao" = %s
+    where c."nomeOperacao"" = %s
     """
 
-    consulta = pd.read_sql(consulta,conn,params=(codOperacao,))
+    consulta = pd.read_sql(consulta,conn,params=(nomeOperacao,))
     conn.close()
 
     return consulta
 
-def InserirOperacao(codOperacao, nomeOperacao, nomeFase, Maq_Equipamento, nomeCategoria, tempoPadrao):
-    consulta = BuscarOperacaoEspecifica(codOperacao)
+def InserirOperacao(nomeOperacao, nomeFase, Maq_Equipamento, nomeCategoria, tempoPadrao):
+    consulta = BuscarOperacaoEspecifica(nomeOperacao)
+    consultaCategoria = CategiaJohnField.BuscarCategorias()
 
     if consulta.empty:
         codFase = FaseJohnField.BuscarFases()
         codFase = codFase[codFase['nomeFase']==nomeFase].reset_index()
+        codCategoria = consultaCategoria[consultaCategoria['nomeCategoria']==nomeCategoria].reset_index()
+
         if codFase.empty:
             return pd.DataFrame([{'Mensagem': f"Fase {nomeFase} nao ´existe!", "status": False}])
         else:
-
             codFase = codFase['codFase'][0]
-
-
             conn = ConexaoPostgreMPL.conexaoJohn()
             inserir = """
-            insert into "Easy"."Operacao" ("codOperacao" , "codFase", "Maq/Equipamento","nomeOperacao") values ( %s, %s,  %s, %s )
+            insert into "Easy"."Operacao" ("codFase", "Maq/Equipamento","nomeOperacao") values (%s,  %s, %s )
             """
             cursor = conn.cursor()
-            cursor.execute(inserir,(codOperacao,int(codFase) ,Maq_Equipamento,nomeOperacao,))
+            cursor.execute(inserir,(int(codFase) ,Maq_Equipamento,nomeOperacao,))
             conn.commit()
+
+
             cursor.close()
+
+            inserirTempoPadrao = """
+            insert into "Easy"
+            """
+
 
             conn.close()
 
