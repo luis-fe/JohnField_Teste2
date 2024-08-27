4b1525d436c9b3e9b16f4571ae83d1594592eb7d
luis-fe <luisengp@gmail.com>
2024-08-27 10:30:54 -0300

    colocando consulta na api registro por periodo

--- routes/Operacoes/ColetaProducao.py
+++ routes/Operacoes/ColetaProducao.py
@@ -72,15 +72,19 @@ def get_RegistroPorPeriodo():
         dataFim = request.args.get('dataFim', '')
         print(dataInicio)
 
-        busca = ColetaProducao.ConsultaRegistroPorPeriodo(codOperador, dataInicio, dataFim)
+        consulta = ColetaProducao.ConsultaRegistroPorPeriodo(codOperador, dataInicio, dataFim)
 
         # Obtém os nomes das colunas
-        column_names = busca.columns.tolist()
-
+        column_names = consulta.columns
         # Monta o dicionário com os cabeçalhos das colunas e os valores correspondentes
-        consulta_data = busca.to_dict(orient='records')
-
+        consulta_data = []
+        for index, row in consulta.iterrows():
+            consulta_dict = {}
+            for column_name in column_names:
+                consulta_dict[column_name] = row[column_name]
+            consulta_data.append(consulta_dict)
         return jsonify(consulta_data)
+
 
 @ColetaProducao_routesJohn.route('/api/JonhField/ApontarAusencia', methods=['POST'])
 @token_required
