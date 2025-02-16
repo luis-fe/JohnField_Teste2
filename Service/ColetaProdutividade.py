8bdec3c7d89b98d27dd0cde6f58acbcd06809ded
luis-fe <luis_engp@hotmail.com>
2025-02-16 11:10:21 -0300

    novo teste

--- Service/ColetaProdutividade.py
+++ Service/ColetaProdutividade.py
@@ -67,6 +67,7 @@ class ColetaProdutividade():
             
             delta1 = self.dataUltimoApontamento_tempo - self.dataHoraApontamento_tempo
             limite_hms = datetime.strptime(self.limiteTempoMinApontamento, "%H:%M:%S")
+            delta1 = timedelta(hours=delta1.hour, minutes=delta1.minute, seconds=delta1.second).total_seconds()
             delta2 = timedelta(hours=limite_hms.hour, minutes=limite_hms.minute, seconds=limite_hms.second).total_seconds()
             # Formatando a saída
             self.dataUltimoApontamento = f"{delta1}|{delta2}"
