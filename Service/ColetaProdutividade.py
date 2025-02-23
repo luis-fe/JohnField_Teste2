77f70d6bbeee80347842f37295e5de6392256981
luis-fe <luis_engp@hotmail.com>
2025-02-23 20:44:46 -0300

    correcao

--- Service/ColetaProdutividade.py
+++ Service/ColetaProdutividade.py
@@ -314,5 +314,5 @@ class ColetaProdutividade():
                 if self.tempoApontamento_tempo > self.horarioTarde_tempo:
                     self.desconto = self.descontoAlmoco + self.desconto
 
-
+                self.validador = 'caso de sexta que aponta seg'
                 self.tempoRealizado = round((self.tempoRealizado-self.desconto)/60,3)
