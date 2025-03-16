b736257edfdc3821f1befea4efb2ce6da929c488
luis-fe <luis_engp@hotmail.com>
2025-03-16 12:03:12 -0300

    alterando par or

--- Service/ColetaProdutividade.py
+++ Service/ColetaProdutividade.py
@@ -585,7 +585,7 @@ class ColetaProdutividade():
 
         # Função para calcular os minutos
         def calcular_minutos(row):
-            if row["Hora"] > row["horaFinal"] and self.dataApontamento != self.dataFinal:
+            if row["Hora"] > row["horaFinal"] or self.dataApontamento != self.dataFinal:
                 return 0
             else:
                 if row["Hora"] < row["horarioManhaFim_"]:
